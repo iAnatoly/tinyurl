32d199d66bbf7b2cb58a86fe650a6dc4d5fad482
iAnatoly <anatoly.ivanov@gmail.com>
2020-08-04 16:07:40 -0700

    ctr mode for randomization

--- generate_id.py
+++ generate_id.py
@@ -2,13 +2,13 @@ from hashlib import sha256
 from base64 import b64encode
 
 class ID:
+    _ctr = 0
     @staticmethod
-    def generate(text, length=1):
-        digest = sha256(bytes(text,"utf-8"))
+    def generate(text, length=10):
+        digest = sha256(bytes("{0}-{1}".format(ID._ctr,text),"utf-8"))
+        ID._ctr += 1
         digest_b64 = b64encode(digest.digest())
         digest_b58 = str(digest_b64.translate(None, b"lIO0+/="),"utf-8")
         if len(digest_b58)<length:
             raise Exception("ID length exhausted")
-
-        print(digest_b58)
         return digest_b58[:length]
