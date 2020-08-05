000556c8f47de5ceb88d390dc54f6c01a5e78c13
iAnatoly <anatoly.ivanov@gmail.com>
2020-08-04 17:17:35 -0700

    mock authorization

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Minimalistric tinyurl
 
-## run a server
+## Run as a local HTTP server
 
 ```
 pip3 install -r requirements.txt
 FLASK_ENV=development python3 server.py
 ```
+
+This will run teh server on :8088. Update server.py to change the port.
 
 ## Storing a URL
 
@@ -13,4 +15,13 @@ FLASK_ENV=development python3 server.py
 curl -XPOST http://localhost:8088/api/v1/url/ --data url=https://localhost/
 
 ```
+
+## Listing all URLs in the datastore
+This method exists just for debug purpose, and requires (mock) authorization
+
+```
+curl http://localhost:8088/api/v1/url/ -H 'Authorization: Bearer bear'
+
+```
+
 
--- tiny.py
+++ tiny.py
@@ -1,23 +1,37 @@
 from flask_restful import Resource, abort, reqparse
-from flask import redirect
+from flask import redirect, request
 
 from generate_id import ID
 from lru import LRU
 
-_datastore = LRU(2, {"123": "https://news.ycombinator.com"})
+_datastore = LRU(2, {'123': 'https://news.ycombinator.com'})
 
 class TinyList(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('url')
 
+    def auth_required(decorated):
+        'mock auth decorator'
+        def wrapper(self):
+            auth_header = request.headers.get('Authorization')
+            if not auth_header: 
+                abort(401, message = 'auth required')
+            (scheme,auth_token) = auth_header.split(' ')
+            if scheme !='Bearer' or auth_token != 'bear': 
+                abort(401, message = 'auth required')
+           
+            return decorated(self)
+        return wrapper
+
+    @auth_required
     def get(self):
         return _datastore, 200
 
     def post(self):
         args = TinyList.parser.parse_args()
-        url = args["url"]
+        url = args['url']
         if url is None:
-            abort(422, message = "unable to process")
+            abort(422, message = 'unable to process')
 
         key = ID.generate(url)
 
@@ -27,7 +41,7 @@ class TinyList(Resource):
                 break
             key = ID.generate(url, len(key)+1)
 
-        _datastore[key] = args["url"]
+        _datastore[key] = args['url']
 
         return { key: _datastore[key]}, 201
 
@@ -41,5 +55,5 @@ class Tiny(Resource):
             return redirect(_datastore[url])
             # return  _datastore[url], 200
 
-        abort(404, message = "URL Not Found")
+        abort(404, message = 'URL Not Found')
 
